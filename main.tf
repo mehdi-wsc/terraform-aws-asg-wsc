95263151d4951bd81bfae443e1da9c896c84d6ae
mehdi-wsc <62596892+mehdi-wsc@users.noreply.github.com>
2020-03-27 09:35:12 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,35 @@
+resource "aws_launch_template" "template" {
+  name = var.name_config
+
+  image_id = var.ami
+
+  instance_type = var.instance_type
+
+  key_name =var.key
+
+  monitoring {
+    enabled = true
+  }
+network_interfaces {
+  associate_public_ip_address =var.ip
+  security_groups =var.security_groups
+}
+
+}
+
+
+
+resource "aws_autoscaling_group" "asg" {
+  name                      = var.name
+  max_size                  = var.max_size
+  min_size                  = var.min_size
+  health_check_grace_period = var.health_check_grace_period
+  health_check_type         = "ELB"
+  desired_capacity          = var.desired_capacity
+  force_delete              = var.force_delete
+  vpc_zone_identifier       = var.vpc_zone_identifier
+  launch_template {
+    id      = aws_launch_template.template.id
+    version = "$Latest"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "name" {}
+variable "max_size" {}
+variable "min_size" {}
+variable "health_check_grace_period" {}
+variable "health_check_type" {}
+variable "desired_capacity" {}
+variable "force_delete" {}
+variable "vpc_zone_identifier" {}
+variable "ami" {}
+variable "instance_type" {}
+variable "key" {}
+variable "ip" {}
+variable "security_groups" {}
+variable "name_config" {}
+
+
+
