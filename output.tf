b982a06d5367f4db3928fb3b6d55842376567564
mehdi <mehdi.kerbedj@wescale.fr>
2020-05-15 03:15:47 +0200

    update output

--- output.tf
+++ output.tf
@@ -1,7 +1,9 @@
 output "id" {
   value = aws_autoscaling_group.asg.id
 }
-
+output "name" {
+  value = aws_autoscaling_group.asg.name
+}
 output "asg_arn" {
   value = aws_autoscaling_group.asg.arn
 }
