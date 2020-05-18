17963563916ae4bdfad3bb0a324cbce3d58e5b56
mehdi <mehdi.kerbedj@wescale.fr>
2020-05-18 16:04:03 +0200

    update

--- test/autoscaling_integration_test.go
+++ test/autoscaling_integration_test.go
@@ -8,14 +8,6 @@ import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
-func TestSyntaxForIntegration(t *testing.T) {
-	terraformOptions := &terraform.Options{
-		TerraformDir: "../example",
-	}
-	terraform.RunTerraformCommandE(t, terraformOptions, terraform.FormatArgs(terraformOptions, "fmt", "--check")...)
-
-}
-
 func TestIntegrationAsg(t *testing.T) {
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../example",
